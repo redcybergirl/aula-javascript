4cab2c1f721c909040fec098df0e2b152a97e452
Giselle <giselle.c298@gmail.com>
2020-05-06 15:48:11 -0300

    Comandos básicos de JavaScript

---
+++ js/main.js
@@ -0,0 +1,155 @@
+/*
+
+function confirme(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por confirmar</b>";
+    //console.log (document.getElementById("agradecimento"))
+    //alert("Obrigado por confirmar");
+}
+*/
+
+/*
+
+Onmouse (over, change, load, out)
+function trocar(elemento){
+    //document.getElementById("agradecimento").innerHTML = "<b>Obrigado por confirmar</b>";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("agradecimento").innerHTML = "<b>Obrigado por confirmar</b>";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página caregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*
+Redirecionamento 
+
+abrir outra aba
+    function redirecionar(){
+    window.open("https://facebook.com/");
+
+abrir na mesma aba
+    function redirecionar(){
+    window.location.href = "https://facebook.com/";
+}
+*/
+
+/*
+Funções
+
+function soma(n1 , n2){
+    return n1 + n2;
+}
+
+var validar;
+function validaIdade(idade){
+    validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar; 
+}
+*/
+
+/*
+Date
+
+var d = new Date();
+alert(d.getDay);
+alert(d.getHours);
+alert(d.getMonth()+1);
+*/
+
+/*
+Laços de repetição white e for
+
+/*var count;
+for(count+0; count <=5; count++){
+    alert(count);
+}
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count +1;
+}
+*/
+
+/*
+Condicional if e else
+
+var idade = prompt("Qual sua idade?");
+if (idade > 18){
+    alert("Maior de idade");
+}else{
+    alert("Menos de idade");
+}
+*/
+
+/*
+Array e dicionário
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+var fruta = {nome:"maçã" , cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+var lista = ["maçã", "pêra", "laranja"];
+*/
+
+/*
+Push, pop e length
+
+lista.push("uva");
+lista.pop();
+*/
+
+/*
+Variáveis 
+
+var nome = "Giselle Caroline";
+var n1 = 5;
+var n2 = 3;
+var frase = "Brasil é o melhor time do mundo";
+console.log(lista);
+*/
+
+/*
+toString para Array
+
+console.log(lista.toString());
+*/
+
+/*
+Join para Array
+
+console.log(lista.join(" - "));
+*/
+
+/*
+Console 
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.tolowerCase());
+*/
+
+/*
+Alertas
+
+alert(frase.replace("Brasil", "Alemanha"));
+alert(nome + "tem" + idade + "anos");
+alert(idade1 + idade2);
+*/
